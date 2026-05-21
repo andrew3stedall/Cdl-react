664a792293d96872218acd75d2720d294899ce3b
andrew3stedall <andrew3stedall@hotmail.com>
2026-05-22 05:44:24 +1000

    fix: format shared contract exports

--- src/cdl_api/contracts/__init__.py
+++ src/cdl_api/contracts/__init__.py
@@ -1,7 +1,17 @@
 """Shared contract exports."""
 
-from cdl_api.contracts.common import ApiErrorResponse, ErrorCode, RuleReference, ValidationErrorResponse
-from cdl_api.contracts.domain import FixtureSummary, GameweekSummary, PlayerSummary, TeamSummary
+from cdl_api.contracts.common import (
+    ApiErrorResponse,
+    ErrorCode,
+    RuleReference,
+    ValidationErrorResponse,
+)
+from cdl_api.contracts.domain import (
+    FixtureSummary,
+    GameweekSummary,
+    PlayerSummary,
+    TeamSummary,
+)
 from cdl_api.contracts.session import SessionState, SessionUser
 from cdl_api.contracts.theme import ThemePreset, UserPreferences
 

525d6f03cf1edc0dd7424403432aba4d7981ad40
andrew3stedall <andrew3stedall@hotmail.com>
2026-07-05 18:20:22 +1000

    Add Artifact Registry Terraform module

---
+++ infra/terraform/modules/artifact-registry/main.tf
@@ -0,0 +1,11 @@
+resource "google_artifact_registry_repository" "this" {
+  project       = var.project_id
+  location      = var.region
+  repository_id = var.repository_id
+  description   = var.description
+  format        = "DOCKER"
+
+  docker_config {
+    immutable_tags = false
+  }
+}

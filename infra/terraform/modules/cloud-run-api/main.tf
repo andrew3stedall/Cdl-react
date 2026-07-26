ff4ac2d0f4bb7a70fe75af9ab21dfe730c1a5152
andrew3stedall <andrew3stedall@hotmail.com>
2026-07-27 09:19:23 +1000

    Add Cloud Run cost labels

--- infra/terraform/modules/cloud-run-api/main.tf
+++ infra/terraform/modules/cloud-run-api/main.tf
@@ -3,6 +3,7 @@ resource "google_cloud_run_v2_service" "this" {
   name     = var.service_name
   location = var.region
   ingress  = "INGRESS_TRAFFIC_ALL"
+  labels   = var.labels
 
   template {
     service_account = var.runtime_service_account_email

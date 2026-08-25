26fe997f8e486862adeceee661b07e2fd7bc0f93
andrew3stedall <andrew3stedall@hotmail.com>
2026-08-25 21:16:52 +1000

    Update src/cdl_api/services/preferences.py

--- src/cdl_api/services/preferences.py
+++ src/cdl_api/services/preferences.py
@@ -15,8 +15,9 @@ SUPPORTED_THEME_PRESETS = {
 }
 SUPPORTED_ATTACK_DIRECTIONS = {"up", "down"}
 SUPPORTED_FDR_DISPLAY_MODES = {"font", "fill"}
-SUPPORTED_POSITION_COLOUR_SCALES = {"Classic", "Ocean", "Vibrant"}
-SUPPORTED_METRIC_COLOUR_SCALES = {"Blue", "Teal", "Purple", "Amber"}
+SUPPORTED_POSITION_COLOUR_SCALES = {"Classic", "Ocean", "Vibrant", "Custom"}
+SUPPORTED_POSITION_COLOUR_MODES = {"name-font", "name-fill", "card-border", "card-fill"}
+SUPPORTED_METRIC_COLOUR_SCALES = {"Blue", "Teal", "Purple", "Amber", "Custom"}
 SUPPORTED_FDR_SCALES = {
     "BrBG",
     "RdBu",
@@ -45,6 +46,7 @@ class UserPreferenceService:
             or preferences.fdr_scale not in SUPPORTED_FDR_SCALES
             or preferences.fdr_display_mode not in SUPPORTED_FDR_DISPLAY_MODES
             or preferences.position_colour_scale not in SUPPORTED_POSITION_COLOUR_SCALES
+            or preferences.position_colour_mode not in SUPPORTED_POSITION_COLOUR_MODES
             or preferences.metric_colour_scale not in SUPPORTED_METRIC_COLOUR_SCALES
             or not THEME_COLOUR_PATTERN.fullmatch(preferences.light_theme_colour)
             or not THEME_COLOUR_PATTERN.fullmatch(preferences.dark_theme_colour)
@@ -53,6 +55,15 @@ class UserPreferenceService:
             or not THEME_COLOUR_PATTERN.fullmatch(preferences.fdr_custom_mid)
             or not THEME_COLOUR_PATTERN.fullmatch(preferences.fdr_custom_fourth)
             or not THEME_COLOUR_PATTERN.fullmatch(preferences.fdr_custom_max)
+            or not THEME_COLOUR_PATTERN.fullmatch(preferences.position_custom_gkp)
+            or not THEME_COLOUR_PATTERN.fullmatch(preferences.position_custom_def)
+            or not THEME_COLOUR_PATTERN.fullmatch(preferences.position_custom_mid)
+            or not THEME_COLOUR_PATTERN.fullmatch(preferences.position_custom_fwd)
+            or not THEME_COLOUR_PATTERN.fullmatch(preferences.metric_custom_1)
+            or not THEME_COLOUR_PATTERN.fullmatch(preferences.metric_custom_2)
+            or not THEME_COLOUR_PATTERN.fullmatch(preferences.metric_custom_3)
+            or not THEME_COLOUR_PATTERN.fullmatch(preferences.metric_custom_4)
+            or not THEME_COLOUR_PATTERN.fullmatch(preferences.metric_custom_5)
         ):
             return self._repository.get_for_user(user_id)
 
